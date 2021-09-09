240701e378c0e8c56892772180723c9076276e98
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-08 22:07:34 -0700

    import store, surround App with Provider, and pass Provider the store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store.js";
+
+//doing these steps of creating a fake reducer for now to get redux standing up on its feet. edit: we took these steps out.
+//howard prefers to build his store in a file called store.
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
