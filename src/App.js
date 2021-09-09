53ed88027760713d3aa6a3998e0e9a0c0fea25af
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-09 00:29:49 -0700

    import login component and render login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { render } from "@testing-library/react";
 import React from "react";
+import Login from "./components/Login.js";
 
 class App extends React.Component {
   render() {
-    return "hello, I'm React";
+    return <Login />;
   }
 }
 
