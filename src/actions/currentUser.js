03da269995ea86f882df335845aca0c478c8e186
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-08 22:01:56 -0700

    create setCurrentUser action creator and begin login action creator

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -9,13 +9,13 @@ export const setCurrentUser = (user) => {
 //asynchronous action creators
 
 export const login = (credentials) => {
-  return dispatch => { 
-      //could dispatch before the fetch something like "loading/getting current user"
-    return fetch("http://localhost:4500/api/v1/login")
-    method: "POST",
-//can dispatch as needed in this fetch. 
-  }
-      
+  return (dispatch) => {
+    //could dispatch before the fetch something like "loading/getting current user"
+    return fetch("http://localhost:4500/api/v1/login");
+    method: "POST";
+    //can dispatch as needed in this fetch.
+  };
+};
 
 //login action will be asynchronous because we need to send a request to our backend in order to login.
 
