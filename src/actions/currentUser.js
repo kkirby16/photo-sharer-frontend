b3a7eabe53bc35c1ecf39c41744918b2b3648239
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-08 21:17:28 -0700

    add actions folder and within it a users file and currentUser file

---
+++ src/actions/currentUser.js
@@ -0,0 +1,22 @@
+//synchronous action creators
+export const setCurrentUser = (user) => {
+  return {
+    type: "SET_CURRENT_USER",
+    user, //shorthand syntax (really user: user)
+  };
+};
+
+//asynchronous action creators
+
+export const login = (credentials) => {
+  return dispatch => { 
+      //could dispatch before the fetch something like "loading/getting current user"
+    return fetch("http://localhost:4500/api/v1/login")
+    method: "POST",
+//can dispatch as needed in this fetch. 
+  }
+      
+
+//login action will be asynchronous because we need to send a request to our backend in order to login.
+
+//this action creator will receive as an argument a user object.
