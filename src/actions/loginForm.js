4886b7842bf1e0fa3c5a646a6a42dbcf3327a7c9
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-10 00:07:14 -0700

    change to take in one argument as formData and change payload key to formData

--- src/actions/loginForm.js
+++ src/actions/loginForm.js
@@ -1,6 +1,6 @@
-export const updateLoginForm = ({ name, value }) => {
+export const updateLoginForm = (formData) => {
   return {
     type: "UPDATE_LOGIN_FORM",
-    payload: formData,
+    formData: formData,
   };
 };
