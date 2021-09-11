0f361b4dc0daf5f7339a9d65d3ce55e85eb7a270
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-11 01:21:00 -0700

    add functionality to get the current user on componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 import { render } from "@testing-library/react";
 import React from "react";
 import Login from "./components/Login.js";
+import { connect } from "react-redux";
+import { getCurrentUser } from "./actions/currentUser.js";
 
 class App extends React.Component {
+  componentDidMount() {
+    this.props.getCurrentUser();
+  }
+
   render() {
     return <Login />;
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App); //we don't need state here but we need this action and we want it to happen everytime the component mounts
