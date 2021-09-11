01824a34690db88b0be96a6098cc2dd134869ba2
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-11 01:18:22 -0700

    add logout component and basic structure for it.

---
+++ src/components/Logout.js
@@ -0,0 +1,21 @@
+//it is wise to build little tiny chunks/representations of our dom in their own components.
+
+//for login forms you can put state for the login into react or into redux.
+
+import React from "react";
+import { connect } from "react-redux";
+import { logout } from "../actions/currentUser.js";
+
+//props get passed into a functional component as an argument and they will also come in as an object.
+const Logout = ({ logout }) => {
+  return (
+    <form onSubmit={logout}>
+      <input type="submit" value="Log Out" />
+    </form>
+  );
+};
+
+export default connect(null, { logout })(Logout);
+//shorthand syntax of simply passing an object, the key is whatever i want to call this object in my component, let's call it updateLoginForm, the value is whatever the value needs to be pointing to usually an action creator. and because key and value are the same, can just use { updateLoginForm }
+//connect returns a function that takes a component and then returns a component.
+//the updateLoginForm action creator you import simply gets passed into connect.
