b95c7c2468c885e5fab9b49d76e409e6453e185e
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-13 19:25:22 -0700

    create basic MainContainer component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainContainer = () => {
+  return (
+    <div className="MainContainer">I am the main container</div> //good idea for the main thing that is being returned from each component to get a div of the name of the component spelled the exact same way.
+  );
+};
+export default MainContainer;
+
+//gonna be where all the action happens probably (like the main thing we are looking at.)
