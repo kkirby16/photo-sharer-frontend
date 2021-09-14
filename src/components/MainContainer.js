711931be375ff81bca2117d1303a1da3a4c613a4
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-13 23:37:27 -0700

    render Posts component passing it a prop called posts

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Posts from "./Posts.js";
 
 const MainContainer = () => {
   return (
-    <div className="MainContainer">I am the main container</div> //good idea for the main thing that is being returned from each component to get a div of the name of the component spelled the exact same way.
+    <div className="MainContainer">
+      <Posts posts={this.props.posts} />
+    </div> //good idea for the main thing that is being returned from each component to get a div of the name of the component spelled the exact same way.
   );
 };
 export default MainContainer;
