c69f7f99ddcf0ad57bae2fc428c8559b04d02221
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-13 22:37:42 -0700

    add case to reducer to reset the login form.

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -7,6 +7,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_LOGIN_FORM":
       return action.formData;
+    case "RESET_LOGIN_FORM":
+      return initialState; //this explicitly needs to return initialState which will always be a cleared out form.
     default:
       return state;
   }
