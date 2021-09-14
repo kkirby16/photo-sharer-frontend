e16894cc97dbd96a5c12cc65b54631b49897db9a
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-13 23:41:11 -0700

    add CLEAR_ALL_POSTS case to allPosts reducer

--- src/reducers/allPosts.js
+++ src/reducers/allPosts.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_ALL_POSTS":
       return action.posts;
+    case "CLEAR_ALL_POSTS":
+      return [];
     default:
       return state; //should I have a different default here since I always want posts to be returned?
   }
