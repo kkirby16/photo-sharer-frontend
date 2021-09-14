d06b231aaffa2fdf123f68261d3d32b2fab79022
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-13 22:31:51 -0700

    have getAllPosts action creator dispatched on mount if there is a current user.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react";
 
 import { connect } from "react-redux";
 import { getCurrentUser } from "./actions/currentUser.js";
+import { getAllPosts } from "./actions/allPosts.js";
+
 import NavBar from "./components/NavBar.js";
 import MainContainer from "./components/MainContainer.js";
 
@@ -12,6 +14,9 @@ class App extends React.Component {
 
   componentDidMount() {
     this.props.getCurrentUser();
+    if (this.props.getCurrentUser() !== null) {
+      this.props.getAllPosts();
+    }
   }
   //need to get the user that getCurrentUser loads into the redux store so App.js has the current user.
 
@@ -31,7 +36,7 @@ class App extends React.Component {
   }
 }
 
-export default connect(null, { getCurrentUser })(App); //we don't need state here but we need this action and we want it to happen everytime the component mounts
+export default connect(null, { getCurrentUser, getAllPosts })(App); //we don't need state here but we need this action and we want it to happen everytime the component mounts
 
 //can also add some routes and might be better to think about sooner rather than later.
 //routing has to do with pointing to different components so we can build all the components at once, show them all on the screen, and then split up the components as I want
