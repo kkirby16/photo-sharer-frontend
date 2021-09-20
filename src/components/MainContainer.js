2d3a0d45167019424a0f9dbeaeb130f1b62cb399
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-20 10:10:43 -0700

    add rendering of Posts component if current user is not equal to null & pass Posts current user in this case

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Posts from "./Posts.js";
 
-const MainContainer = () => {
-  return (
-    <div className="MainContainer">
-      <Posts posts={this.props.posts} />
-    </div> //good idea for the main thing that is being returned from each component to get a div of the name of the component spelled the exact same way.
-  );
+const MainContainer = (props) => {
+  console.log("props:", props.posts);
+  if (props.currentUser !== null) {
+    return (
+      <div className="MainContainer">
+        <Posts posts={props.posts} currentUser={props.currentUser} />
+      </div> //good idea for the main thing that is being returned from each component to get a div of the name of the component spelled the exact same way.
+    );
+  } else return null;
 };
 export default MainContainer;
 
