c34d68518252a5b073ad84d7ccc4c732c37b7e65
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-27 20:58:44 -0700

    add commentInput and signupForm reducers to the store.

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import usersReducer from "./reducers/users.js"; //because default export we can choose to name it whatever we want.
 import currentUser from "./reducers/currentUser.js";
 import allPosts from "./reducers/allPosts.js";
+import commentInput from "./reducers/commentInput";
+import signupForm from "./reducers/signupForm.js";
 
 import thunk from "redux-thunk"; //i imported this without the curly braces to get it to work. is that okay?
 import loginForm from "./reducers/loginForm.js";
@@ -11,6 +13,8 @@ const reducer = combineReducers({
   currentUser,
   loginForm,
   allPosts,
+  commentInput,
+  signupForm,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //**this line and the composeEnhancer part below is kind of a newer version of how we have to put our middleware together. and this is probably the way you're going to need to do this if you are building everything from scratch now. so it is just a minor change in the syntax of what redux needs.
