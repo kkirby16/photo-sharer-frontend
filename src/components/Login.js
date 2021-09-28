f5dbfa0bc8474d3cf845686d444aa9fb7439f516
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-27 20:44:10 -0700

    add url change functionality after successful login, and routing functionality.

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,42 +4,46 @@ import React from "react";
 import { connect } from "react-redux";
 import { updateLoginForm } from "../actions/loginForm.js";
 import { login } from "../actions/currentUser.js";
+import { Link } from "react-router-dom";
 
 //props get passed into a functional component as an argument and they will also come in as an object.
-const Login = ({ loginFormData, updateLoginForm, login }) => {
+const Login = ({ loginFormData, updateLoginForm, login, history }) => {
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     const updatedFormInfo = {
       ...loginFormData, //the spreading here of the loginForm keeps the other property not currently being updated in place.
       [name]: value, //why have the brackets around name?
     };
-    updateLoginForm(updatedFormInfo);
+    updateLoginForm(updatedFormInfo, history);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    login(loginFormData);
+    login(loginFormData, history); //need to pass along more than the loginFormData here... need my history to change once successfully logged in the url should go back to like "/" or something else.
   };
 
   return (
     //the third destructured variable above is a beefed up reduxed version of that action creator
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="username"
-        value={loginFormData.username}
-        name="username"
-        type="text"
-        onChange={handleInputChange}
-      />
-      <input
-        placeholder="password"
-        value={loginFormData.password} //our value makes this a controlled input.
-        name="password"
-        type="text" //the below is already the event in the () so can pass name and value from the event
-        onChange={handleInputChange} //<-- when we have a callback to an event we automatically get the event as an argument.
-      />
-      <input type="submit" value="Log In" />
-    </form>
+    <div>
+      <Link to="/">Back to home</Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="username"
+          value={loginFormData.username}
+          name="username"
+          type="text"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="password"
+          value={loginFormData.password} //our value makes this a controlled input.
+          name="password"
+          type="text" //the below is already the event in the () so can pass name and value from the event
+          onChange={handleInputChange} //<-- when we have a callback to an event we automatically get the event as an argument.
+        />
+        <input type="submit" value="Log In" />
+      </form>
+    </div>
   );
 }; //name will keep track of this when we change things.
 
