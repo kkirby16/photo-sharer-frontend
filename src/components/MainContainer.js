91b45c7b5cd3c6c481a3ef87d122de7ced3aaba4
kkirby16 <kkirby16@my.whitworth.edu>
2021-10-01 14:37:28 -0700

    styling changes & adding of mapStateToProps functionality

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,18 +2,28 @@ import React from "react";
 import Posts from "./Posts.js";
 import PostInput from "./PostInput.js";
 import Home from "./Home.js";
+import "./myStyles.css";
+import { connect } from "react-redux";
 
 const MainContainer = (props) => {
   console.log("props:", props.posts);
   if (props.currentUser !== null) {
     return (
       <div className="MainContainer">
-        <Posts posts={props.posts} currentUser={props.currentUser} />
         <PostInput addPost={props.addPost} currentUser={props.currentUser} />
+        <br></br>
+        <br></br>
+        <Posts posts={props.posts} currentUser={props.currentUser} />
       </div> //good idea for the main thing that is being returned from each component to get a div of the name of the component spelled the exact same way.
     );
   } else return <Home />;
 };
-export default MainContainer;
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.allPosts.posts,
+  };
+};
+export default connect(mapStateToProps)(MainContainer);
 
 //gonna be where all the action happens probably (like the main thing we are looking at.)
