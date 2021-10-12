826d0447cb082e29f71e79a85cea7846b8176d46
kkirby16 <kkirby16@my.whitworth.edu>
2021-10-11 22:01:35 -0700

    change clear post & add comment cases, & add cases for clear comment & liking/unliking

--- src/reducers/allPosts.js
+++ src/reducers/allPosts.js
@@ -1,6 +1,7 @@
 export default (state = { posts: [] }, action) => {
   switch (action.type) {
     case "SET_ALL_POSTS":
+      console.log("WHAT'S STATE HERE?", state.posts);
       return { ...state, posts: [...state.posts, ...action.posts] };
     case "CLEAR_ALL_POSTS":
       return { ...state, posts: [] };
@@ -11,20 +12,44 @@ export default (state = { posts: [] }, action) => {
       return { ...state, posts: [action.post, ...state.posts] };
     // return [action.post, ...state];
     case "CLEAR_POST":
-      return state.filter((post) => post.id !== action.postId);
+      return {
+        ...state,
+        posts: [...state.posts.filter((post) => post.id !== action.postId)],
+      };
     case "ADD_COMMENT":
       let newPosts = [...state.posts].map((post) => {
         if (parseInt(post.id) === action.payload.post_id) {
-          post.attributes.comments = [
-            ...post.attributes.comments,
-            action.payload,
-          ];
-          return post;
+          let updatedPost = {
+            ...post,
+            comments: [...post.comments, action.payload],
+          };
+          return updatedPost;
         } else {
           return post;
         }
       });
       return { ...state, posts: newPosts };
+    case "CLEAR_COMMENT":
+      let post = state.posts.find((post) => post.id == action.postId); //had to do a complicated delete like this because comment was nested under post
+      let postIdx = state.posts.findIndex((post) => post.id == action.postId);
+
+      // let commentIdx = post.comments.findIndex(
+      //   (comment) => comment.id === action.commentId
+      // );
+
+      let postComments = post.comments.filter(
+        (comment) => comment.id !== action.commentId
+      );
+      post.comments = postComments;
+
+      return {
+        ...state,
+        posts: [
+          ...state.posts.slice(0, postIdx),
+          Object.assign({}, post), //this is putting the newly updated post into a new object to be super cler with redux that there is a change in state (change of the post/less comments) so re-render happens
+          ...state.posts.slice(postIdx + 1),
+        ],
+      };
 
     // let newPosts = state.map((post) => {
     //   // console.log("ACTION", post);
@@ -45,6 +70,45 @@ export default (state = { posts: [] }, action) => {
     //   }
     // });
     // return newPosts;
+    case "ADD_LIKE":
+      console.log("What is the action", action.payload);
+      let posts = [...state.posts].map((post) => {
+        if (parseInt(post.id) === action.payload.post_id) {
+          let updatedPost = {
+            ...post,
+            likes: [...post.likes, action.payload],
+          };
+          console.log("UPDATED POST", updatedPost);
+
+          return updatedPost;
+        } else {
+          return post;
+        }
+      });
+      return { ...state, posts: posts };
+
+    case "DELETE_LIKE":
+      console.log("deleted_like", action);
+      let unlikedPost = state.posts.find((post) => post.id == action.post_id);
+      console.log("UNLIKED POST", unlikedPost);
+      let postIndex = state.posts.findIndex(
+        (post) => post.id == action.post_id
+      );
+
+      let postLikes = unlikedPost.likes.filter(
+        (like) => like.id !== action.users_like.id
+      );
+      unlikedPost.likes = postLikes;
+
+      return {
+        ...state,
+        posts: [
+          ...state.posts.slice(0, postIndex),
+          Object.assign({}, unlikedPost),
+          ...state.posts.slice(postIndex + 1),
+        ],
+      };
+
     default:
       return state; //should I have a different default here since I always want posts to be returned?
   }
