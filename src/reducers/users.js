fcb47a1490eff32c9351a32178977fc3d8e93fa1
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-11 13:19:18 -0800

    take out comments

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,10 @@
 export default (state = [], action) => {
-  //can be more specific with your initial state depending on what it is... if it is an object you can be more specific by having an iniital state variable set to an object.
+  //can be more specific with your initial state depending on what it is... if it is an object you can be more specific by having an initial state variable set to an object.
   switch (action.type) {
     default:
       return state;
   }
-}; //since this is users reducer, it will return an array of users.
+}; //since this is the users reducer, it will return an array of users.
 
 //when @@INIT gets fired it is going to return the default state of all of your reducers.
 //default state should be an empty version of whatever this reducer is responsible for returning.
