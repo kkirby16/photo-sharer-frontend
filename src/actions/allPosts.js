aa010a6a15627c600fe461cc132012637da1c4b4
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-14 20:24:12 -0800

    take out a header in a fetch request

--- src/actions/allPosts.js
+++ src/actions/allPosts.js
@@ -18,7 +18,6 @@ export const getAllPosts = () => {
         "Access-Control-Allow-Credentials": true,
         "Access-Control-Allow-Origin":
           "https://photo-sharer-kkirby16.netlify.app",
-        "Access-Control-Allow-Methods": "GET",
       },
     })
       .then((r) => r.json())
