ff820345046bfb26abee2c418ddd8b19557a88f3
kkirby16 <kkirby16@my.whitworth.edu>
2021-12-26 22:13:12 -0800

    import useRef, useEffect, & gsap, change function component syntax, and add animations to site heading with useRef and useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,86 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./myStyles.css";
 
-const Home = ({}) => (
-  <div className="backgroundImage">
-    <h2 className="greeting">
-      <li className="no_bullet_point_lis home_page_title_text ">P h o t o </li>
-      <li className="no_bullet_point_lis home_page_title_text ">
-        S h a r e r{" "}
-      </li>
-      <br></br>
-      <br></br>
-      <li className="no_bullet_point_lis home_page_text">
-        Post photos with captions for others to see
-      </li>
-      <li className="no_bullet_point_lis home_page_text">
-        See photos others have posted
-      </li>
-      <li className="no_bullet_point_lis home_page_text">
-        Comment on and like posts to{" "}
-        <span className="interact_word">interact</span>{" "}
-      </li>
-      <br></br>
-      <br></br>
-      <Link
-        to="/signup"
-        style={{
-          fontFamily: "Recoleta Alt",
-          color: "white",
-          fontSize: "53px",
-          textDecoration: "none",
-          position: "relative",
-          top: "-18px",
-        }}
-        className="signupAndLoginText"
-      >
-        sign up
-      </Link>{" "}
-      <span className="or_text">&nbsp;&nbsp; or &nbsp;&nbsp;</span>{" "}
-      <Link
-        to="/login"
-        style={{
-          fontFamily: "Recoleta Alt",
-          color: "white",
-          fontSize: "53px",
-          textDecoration: "none",
-          position: "relative",
-          top: "-18px",
-        }}
-        className="signupAndLoginText"
-      >
-        log in
-      </Link>
-    </h2>
-  </div>
-);
+import { gsap } from "gsap";
+
+function Home() {
+  let siteHeading = useRef(null);
+
+  useEffect(() => {
+    gsap.to(siteHeading, {
+      duration: 0.8,
+      opacity: 1,
+      y: -20,
+      ease: "power3",
+    });
+  }, []);
+
+  return (
+    <div className="backgroundImage">
+      <h2 className="greeting">
+        <li
+          ref={(el) => {
+            siteHeading = el;
+          }}
+          className="no_bullet_point_lis home_page_title_text "
+        >
+          P h o t o{" "}
+        </li>
+        <li
+          ref={(el) => {
+            siteHeading = el;
+          }}
+          className="no_bullet_point_lis home_page_title_text "
+        >
+          S h a r e r{" "}
+        </li>
+        <br></br>
+        <br></br>
+        <li className="no_bullet_point_lis home_page_text">
+          Post photos with captions for others to see
+        </li>
+        <li className="no_bullet_point_lis home_page_text">
+          See photos others have posted
+        </li>
+        <li className="no_bullet_point_lis home_page_text">
+          Comment on and like posts to{" "}
+          <span className="interact_word">interact</span>{" "}
+        </li>
+        <br></br>
+        <br></br>
+        <Link
+          to="/signup"
+          style={{
+            fontFamily: "Recoleta Alt",
+            color: "white",
+            fontSize: "53px",
+            textDecoration: "none",
+            position: "relative",
+            top: "-18px",
+          }}
+          className="signupAndLoginText"
+        >
+          sign up
+        </Link>{" "}
+        <span className="or_text">&nbsp;&nbsp; or &nbsp;&nbsp;</span>{" "}
+        <Link
+          to="/login"
+          style={{
+            fontFamily: "Recoleta Alt",
+            color: "white",
+            fontSize: "53px",
+            textDecoration: "none",
+            position: "relative",
+            top: "-18px",
+          }}
+          className="signupAndLoginText"
+        >
+          log in
+        </Link>
+      </h2>
+    </div>
+  );
+}
 
 export default Home;
