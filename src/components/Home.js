ac773b976ea6cf892bbd5c49af06b1ad763c1a08
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-30 22:18:21 -0800

    change animation of descriptive text

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,9 +26,9 @@ function Home() {
       delay: 0.1,
     });
     gsap.from(descriptiveText, {
-      duration: 0.87,
+      duration: 0.95,
       x: "-100vw",
-      delay: 0.15,
+      delay: 0.12,
       ease: "power2.in",
     });
     gsap.to(signupText, {
