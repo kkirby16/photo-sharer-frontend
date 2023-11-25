c004b180d6c88e364646fb00f20c02d574fc43dd
kkirby16 <kkirby16@my.whitworth.edu>
2023-11-25 00:22:40 -0800

    add Material UI tooltip for logout button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,17 +5,26 @@ import { connect } from "react-redux";
 import { logout } from "../actions/currentUser.js";
 import "./myStyles.css";
 import { Icon } from "@iconify/react";
+import Tooltip from "@mui/material/Tooltip";
+import Fade from "@material-ui/core/Fade";
 
 //props get passed into a functional component as an argument and they will also come in as an object.
 const Logout = ({ logout }) => {
   return (
-    <Icon
-      icon="teenyicons:logout-solid"
-      width="25"
-      height="25"
-      onClick={logout}
-      className="logoutButton"
-    />
+    <Tooltip
+      title="Logout"
+      placement="left"
+      TransitionComponent={Fade}
+      TransitionProps={{ timeout: 0 }}
+    >
+      <Icon
+        icon="teenyicons:logout-solid"
+        width="25"
+        height="25"
+        onClick={logout}
+        className="logoutButton"
+      />
+    </Tooltip>
   );
 };
 
