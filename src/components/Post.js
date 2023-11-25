27deb8afa072f714cf3e031d122f4926b9b8c728
kkirby16 <kkirby16@my.whitworth.edu>
2023-11-25 01:53:12 -0800

    add Material UI tooltip for like button and add Material UI tooltip for button that lets a user delete their post

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,6 +8,8 @@ import CommentInput from "./CommentInput.js";
 import { connect } from "react-redux";
 import "./myStyles.css";
 import { Icon } from "@iconify/react";
+import Tooltip from "@mui/material/Tooltip";
+import Fade from "@material-ui/core/Fade";
 
 class Post extends Component {
   can_delete_post = () => {
@@ -33,15 +35,22 @@ class Post extends Component {
       )
     ) {
       return (
-        <Icon //like button
-          icon="fluent:heart-20-regular"
-          width="30"
-          height="30"
-          className="likeButton"
-          onClick={() =>
-            this.props.addLike(this.props.currentUser.id, this.props.post.id)
-          }
-        />
+        <Tooltip
+          title="Like post"
+          placement="right-start"
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 0 }}
+        >
+          <Icon //like button
+            icon="fluent:heart-20-regular"
+            width="30"
+            height="30"
+            className="likeButton"
+            onClick={() =>
+              this.props.addLike(this.props.currentUser.id, this.props.post.id)
+            }
+          />
+        </Tooltip>
       );
     } else {
       return (
@@ -79,18 +88,25 @@ class Post extends Component {
           <div className="postInfo">
             {this.handle_likes()}
             {this.can_delete_post() === true ? (
-              <Icon
-                icon="octicon:trash-24"
-                width="23"
-                height="23"
-                className="deleteButton"
-                type="button"
-                onClick={() => {
-                  this.props.removePost(this.props.post.id);
-                }}
+              <Tooltip
+                title="Delete post"
+                placement="right-start"
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 0 }}
               >
-                {" "}
-              </Icon>
+                <Icon
+                  icon="octicon:trash-24"
+                  width="23"
+                  height="23"
+                  className="deleteButton"
+                  type="button"
+                  onClick={() => {
+                    this.props.removePost(this.props.post.id);
+                  }}
+                >
+                  {" "}
+                </Icon>
+              </Tooltip>
             ) : null}
             <li className="likesAmount">
               {this.props.post.likes.length == 1
